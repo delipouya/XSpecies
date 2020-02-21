6819136da6fc6ba9943541613d98910e66771877
delipouya <d.pouyabahar@mail.utoroto.ca>
2020-02-20 19:37:23 -0500

    check

---
+++ Codes/label_by_DE_results.R
@@ -0,0 +1,4 @@
+## Expand the markers based on DE results + curated markers > check if the labeling is improved
+## try out Grannett as well
+
+MappedGenesToOrthologs <- readRDS( 'Data/McParland_markers/mapped_markers_hs2ratRnor/All_mapped_markers_hs2ratRnor_list.rds')
