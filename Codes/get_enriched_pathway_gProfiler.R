c612ec4bc75542e5d60ba490de24a5b8f653903f
Delaram Pouyabahar <d.pouyabahar@gmail.com>
2020-03-29 19:12:26 -0400

    .

--- Codes/get_enriched_pathway_gProfiler.R
+++ Codes/get_enriched_pathway_gProfiler.R
@@ -25,20 +25,42 @@ RES= ''
 if(INPUT_NAME=='rat_Rnor') RES = '_res.1'
 if(INPUT_NAME=='rat_Lew_01')  RES = '_res.0.8' 
 
+Cluster_markers_merged <- readRDS('~/Desktop/cell_type_markers/rat_Rnor/markers_rat_Rnor_mito_50_lib_1500_res.1.rds')
 Cluster_markers_merged <- readRDS(paste0('Results/', INPUT_NAME, '/markers/markers_', 
                                          OUTPUT_NAME, RES,'.rds'))
 cluster_names <- names(Cluster_markers_merged)
 lapply(Cluster_markers_merged, dim)
 
+######## enrichment using gProfiler
+gp_enrich_res <- sapply(1:length(Cluster_markers_merged), 
+                        function(i){
+                          markers = Cluster_markers_merged[[i]]$ensemble_ids
+                          enrich_res = get_gprofiler_enrich(markers, model_animal_name);return(enrich_res)
+                          },simplify = F)
+names(gp_enrich_res) <- names(Cluster_markers_merged)
+
+lapply(gp_enrich_res, function(x) head(x$result))
+saveRDS(gp_enrich_res,'~/Desktop/cell_type_markers/rat_Rnor/gProfiler_rat_Rnor_mito_50_lib_1500_res.1.rds')
 
 
-sapply(1:length(Cluster_markers_merged), get_gprofiler_enrich(x[[i]]$ensemble_ids, model_animal_name)
-gostres$result = gostres$result[gostres$result$query_size>5 & gostres$result$query_size<350 & 
-                                  gostres$result$intersection_size >3, ]
-head(gostres$result,20)
-
+gp_enrich_res_filt <- lapply(gp_enrich_res, 
+                            function(x) x$result[x$result$query_size>5 & x$result$query_size<350 & x$result$intersection_size >3, ])
+names(gp_enrich_res_filt)  
+lapply(gp_enrich_res_filt, head)
+head(gp_enrich_res_filt[['cluster_10']], 10)
 
 p <- gostplot(gostres, capped = T, interactive = FALSE)
 # pt2 <- publish_gosttable(gostres, use_colors = TRUE, filename = NULL)
+
+
+### ToDo
+### do the same analysis in GSEA 
+#### visualize using enrichmentMap and cytoscape
+#### check the paper for known markers + check the presentation data
+#### annotate the DA-01 dataset
+
+## need to find gene set 
+# http://www.go2msig.org/cgi-bin/prebuilt.cgi
+
 
 
